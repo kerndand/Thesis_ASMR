7108c59836742d622097aefb01b9c4a2b33222ec
Daniel Kern <37657554+kerndand@users.noreply.github.com>
2020-10-16 15:59:28 +0200

    test

--- script.ts
+++ script.ts
@@ -1,11 +1,11 @@
 namespace ASMR_Script{
     window.addEventListener("load", handleLoad);
-    let demographics: HTMLFormElement;  
+    let demographics: HTMLDivElement;  
 
    function handleLoad(_event: Event): void {
         
 
-        demographics = <HTMLFormElement>document.querySelector("form");        
+        demographics = <HTMLDivElement>document.querySelector("div#demographics");        
         let button = <HTMLButtonElement>document.querySelector("button");
 
         demographics.addEventListener("change", handleChange);
@@ -13,14 +13,20 @@ namespace ASMR_Script{
     }
 
     function handleChange(_event: Event): void { 
-        let formData: FormData = new FormData(demographics);     
+        let formData: FormData = new FormData(document.querySelector("form"));     
         for (let entry of formData) {
             console.log(entry);
         }
     }
 
     function handleClick(_event: Event): void {
-       demographics.style.visibility = "hidden";
+       demographics.style.display = "none";
+
+       let fieldset: HTMLFieldSetElement = document.createElement("fieldset");
+       demographics.appendChild(fieldset);
+       let legend = document.createElement("legend");
+       fieldset.appendChild(legend);
+       legend.innerHTML="Wie haben Sie sich beim Hören gefühlt? (Adjektiv)"
     }
 
 }
