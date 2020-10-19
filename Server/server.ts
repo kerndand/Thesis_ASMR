c4a14f701ba70bde1916739611fdf3ee197dfc98
Daniel Kern <37657554+kerndand@users.noreply.github.com>
2020-10-19 17:42:30 +0200

    server

---
+++ Server/server.ts
@@ -0,0 +1,37 @@
+import * as Http from "http";
+import * as Url from "url";
+
+namespace ASMR_Script {
+   let server: Http.Server = Http.createServer();
+
+   let port: number | string | undefined = process.env.PORT;
+
+   if (port == undefined){
+       port = 5001;
+   }
+   
+   console.log("Server starting on port: " + port);
+   server.listen(port);
+   server.addListener("request", handleRequest);
+
+   function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        console.log("Ich bin da");
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+
+        if (_request.url){
+            let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+            for (let key in url.query){
+                _response.write(key + ":" + url.query[key] + "<br>");
+            }
+            
+            let jsonString: string = JSON.stringify(url.query);
+            _response.write(jsonString);
+        }
+
+
+       _response.write("Vielen Dank für Ihre Teilnahme!");
+       _response.end();
+   }
+
+}
