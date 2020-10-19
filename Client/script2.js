8e0715c283cff12c962e05c73367441f8dcae9e0
Daniel Kern <37657554+kerndand@users.noreply.github.com>
2020-10-19 22:21:35 +0200

    heroku

--- Client/script2.js
+++ Client/script2.js
@@ -1,30 +1,20 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var ASMR_Script;
 (function (ASMR_Script) {
     window.addEventListener("load", handleLoad);
-    let form;
+    //let form: HTMLFormElement;
     // Load-Funktion
     function handleLoad(_event) {
         ASMR_Script.generateContent();
-        form = document.querySelector("form");
-        let submit = document.querySelector("button[type=button]");
-        submit.addEventListener("click", sendForms);
+        /*  form = <HTMLFormElement>document.querySelector("form");
+         let submit: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button[type=button]");
+ 
+         submit.addEventListener("click", sendForms);  */
     }
-    function sendForms() {
-        return __awaiter(this, void 0, void 0, function* () {
-            let formData = new FormData(form);
-            let query = new URLSearchParams(formData);
-            yield fetch("main.html?" + query.toString());
-            alert("Data send!");
-        });
-    }
+    /*   async function sendForms(): Promise<void>{
+          let formData: FormData = new FormData(form);
+          let query: URLSearchParams = new URLSearchParams(<any>formData);
+          await fetch("main.html?" + query.toString());
+          alert("Data send!");
+      }  */
 })(ASMR_Script || (ASMR_Script = {}));
 //# sourceMappingURL=script2.js.map
